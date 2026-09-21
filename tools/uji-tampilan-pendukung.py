3e96bf855db0578e747ab58458438f08161903f0
arapcihuy <rasyidahmad180@gmail.com>
2026-09-21 22:15:42 +0700

    feat(seo): artikel contoh soal TIU CPNS, tautan silang TWK, halaman masuk uji tampilan

--- tools/uji-tampilan-pendukung.py
+++ tools/uji-tampilan-pendukung.py
@@ -36,7 +36,7 @@ PORT = 8791
 HAL = ['mutu/', 'beli/', 'syarat/', 'privasi/', 'contoh/', '404.html',
        'contoh-soal-psikotes-kerja/', 'contoh-soal-psikotes-matematika/',
        'contoh-soal-deret-angka/', 'contoh-soal-tes-kraepelin/',
-       'contoh-soal-twk-cpns/']
+       'contoh-soal-twk-cpns/', 'contoh-soal-tiu-cpns/']
 RUSAK = '--rusak' in sys.argv   # mode pembuktian: uji HARUS menangkap kerusakan yang disuntikkan
 hasil = []
 
